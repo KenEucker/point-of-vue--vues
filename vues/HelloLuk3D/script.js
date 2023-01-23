f55b81dffd075e986a092b91772b1062bdd2d2e8
Ken Eucker <keneucker@gmail.com>
2023-01-22 22:06:37 -0800

    for demo

---
+++ vues/HelloLuk3D/script.js
@@ -0,0 +1,16 @@
+import fs from 'fs'
+
+const vuesHeading = `These are the names of my ${vue.name} vues`
+const imagesHeading = 'Here are my images'
+
+alert('hiiii')
+
+const alert = "this is my alert message"
+
+window.alert('hiiii')
+
+console.log('alert', alert)
+
+someFun(alert)
+
+someFun(window)
