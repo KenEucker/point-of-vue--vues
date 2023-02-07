25b8cf29e85aa851b543367b96054024baadf210
Ken Eucker <keneucker@gmail.com>
2023-02-06 16:08:18 -0800

    updates HelloWorld [vundefined]

--- vues/HelloWorld/template.vue
+++ vues/HelloWorld/template.vue
@@ -1,7 +1,7 @@
 <h1>Hello, {{ msg }}</h1>
 <div>
     <h2>Your Posts</h2>
-    <div v-for="post in query.viewer.posts">
+    <div v-for="post in query.posts">
         {{ post.tite}}
     </div>
 </div>
